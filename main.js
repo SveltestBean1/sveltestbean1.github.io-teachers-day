a3239a4681dbbaad3c223d8bde57814141979b8c
SveltestBean1 <SveltestBean1@users.noreply.github.com>
2019-09-03 23:12:40 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(function(){alert("Working!");
+$(document).ready(function(){alert("Working!");
 var url = window.location.href;
 var stuff = url.substring(45, substring.length());
 alert(stuff);})
